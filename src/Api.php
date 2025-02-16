c262531f0c284e73bfa6a74fed36e22443cfcefe
kkonshin <kkonshin@gmail.com>
2025-02-16 23:25:58 +0300

    Public API update

--- src/Api.php
+++ src/Api.php
@@ -8,15 +8,7 @@ class Api
 {
     public function json(): array
     {
-        $context = stream_context_create(
-            array(
-                "http" => array(
-                    "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
-                )
-            )
-        );
-
-        $json = file_get_contents('https://laravel-news.com/feed/json', false, $context);
+        $json = file_get_contents('https://pokeapi.co/api/v2/pokemon/ditto');
 
         return json_decode($json, true);
     }
