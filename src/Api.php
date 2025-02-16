a57985e0738d6c354e86358da672ad0afa655612
kkonshin <kkonshin@gmail.com>
2025-02-16 23:20:13 +0300

    Context header added

--- src/Api.php
+++ src/Api.php
@@ -8,7 +8,15 @@ class Api
 {
     public function json(): array
     {
-        $json = file_get_contents('https://laravel-news.com/feed/json');
+        $context = stream_context_create(
+            array(
+                "http" => array(
+                    "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+                )
+            )
+        );
+
+        $json = file_get_contents('https://laravel-news.com/feed/json', false, $context);
 
         return json_decode($json, true);
     }
